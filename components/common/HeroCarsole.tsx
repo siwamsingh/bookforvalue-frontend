473e8b37c9e40426392f0c0376982fc050313c95
siwamsingh <siwamsingh2003@gmail.com>
2026-02-16 18:06:47 +0530

    updated Carsole

--- components/common/HeroCarsole.tsx
+++ components/common/HeroCarsole.tsx
@@ -2,11 +2,18 @@
 import Image, { StaticImageData } from "next/image";
 import React, { useEffect, useState } from "react";
 
+import MobileHero1 from "@/assets/HeroCarsole/heroimg1.png"
+import MobileHero2 from "@/assets/HeroCarsole/heroimg2.png"
+import MobileHero3 from "@/assets/HeroCarsole/heroimg3.png"
+import MobileHero4 from "@/assets/HeroCarsole/heroimg4.png"
+
 interface HeroCarouselProps {
   images: StaticImageData[];
   autoSlide?: boolean;
   autoSlideInterval?: number;
 }
+
+const smallImages = [MobileHero1, MobileHero2, MobileHero3, MobileHero4]
 
 const HeroCarousel: React.FC<HeroCarouselProps> = ({
   images,
@@ -38,7 +45,7 @@ const HeroCarousel: React.FC<HeroCarouselProps> = ({
   }, [currentIndex, autoSlide, autoSlideInterval]);
 
   return (
-    <div className="relative w-full max-w-7xl mx-auto mt-6">
+    <div className="relative w-full max-w-7xl mx-auto ">
       <div className="relative overflow-hidden">
         {/* Slides */}
         <div
@@ -52,7 +59,16 @@ const HeroCarousel: React.FC<HeroCarouselProps> = ({
               key={index}
               src={image}
               alt={`slide-${index}`}
-              className="w-full bg-gray-900 flex-shrink-0 object-contain"
+              className="w-full bg-gray-900 flex-shrink-0 hidden sm:block object-contain"
+            />
+          ))}
+
+          {smallImages.map((image, index) => (
+            <Image
+              key={index}
+              src={image}
+              alt={`slide-${index}`}
+              className="w-full bg-gray-900 flex-shrink-0 block sm:hidden object-contain"
             />
           ))}
         </div>
